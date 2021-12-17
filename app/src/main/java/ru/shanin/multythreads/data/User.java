a461fb0fa90dca0958f5c3d348691edc9e65bd75
redhatdeath <redhatdeath@yandex.ru>
2021-12-17 19:42:14 +0500

    Initial commit

---
+++ app/src/main/java/ru/shanin/multythreads/data/User.java
@@ -0,0 +1,43 @@
+package ru.shanin.multythreads.data;
+
+import java.util.Random;
+
+public class User {
+
+    private final String firstName, secondName;
+
+    private final static int firstNameLength, secondNameLength;
+
+    static {
+        firstNameLength = 8;
+        secondNameLength = 12;
+    }
+
+    public User() {
+        this.firstName = GenData.getNextRandomString(firstNameLength);
+        this.secondName = GenData.getNextRandomString(secondNameLength);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+
+    static class GenData {
+
+        static String getNextRandomString(int length) {
+            String strChar = "abcdefghijklmnopqrstuvwxyz";
+            Random random = new Random();
+            StringBuilder sbRandomString = new StringBuilder(length);
+            for (int i = 0; i < length; i++) {
+                int randomInt = random.nextInt(strChar.length());
+                sbRandomString.append(strChar.charAt(randomInt));
+            }
+            return sbRandomString.toString();
+        }
+    }
+}
