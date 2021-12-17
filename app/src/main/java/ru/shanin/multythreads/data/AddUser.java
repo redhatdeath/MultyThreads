f8b10c9bd56615633bb9e5e372d0c82b5c270972
redhatdeath <redhatdeath@yandex.ru>
2021-12-17 20:51:38 +0500

    no threads worker

--- app/src/main/java/ru/shanin/multythreads/data/AddUser.java
+++ app/src/main/java/ru/shanin/multythreads/data/AddUser.java
@@ -1,22 +1,27 @@
 package ru.shanin.multythreads.data;
 
+import timber.log.Timber;
+
 public class AddUser implements Runnable {
     private boolean running;
     private int count;
 
     public AddUser(String threadName) {
         Thread.currentThread().setName(threadName);
+        Timber.d("threadName = " + threadName);
     }
 
     @Override
     public void run() {
         while (running) {
-            Worker.addUserToQueue(new User(Thread.currentThread().getName()));
-            try {
-                Thread.sleep((int) (Math.random() * 51 + 50));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            User user = new User(Thread.currentThread().getName());
+            Timber.d("user = " + user.toString());
+            Worker.addUserToQueue(user);
+//            try {
+//                Thread.sleep((int) (Math.random() * 51 + 50));
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
             count++;
             if (count > 50) running = false;
         }
